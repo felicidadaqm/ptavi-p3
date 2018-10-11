f66b816e2b12e0fc0f006ac86363ecbbf39844de
Felicidad <felicidadaqm@gmail.com>
2018-10-12 00:54:54 +0200

    A medio hacer el EJ7

--- karaoke.py
+++ karaoke.py
@@ -7,11 +7,23 @@ import sys
 from collections import OrderedDict
 import json
 from urllib.request import urlretrieve
-import urllib
 
-class Print (smallsmilhandler.smallSMILHandler):
+class KaraokeLocal():
 
-    def print_line(self): 
+    def __init__(self):
+        parser = make_parser()
+        cHandler = smallsmilhandler.smallSMILHandler()
+        parser.setContentHandler(cHandler)
+        self.tag = cHandler.get_tags()
+
+        file = sys.argv[1]
+        try:
+            parser.parse(open(file))
+        except (FileNotFoundError):
+            sys.exit("Usage: python3 karaoke.py file.smil")
+
+
+    def __str__(self): 
         exit = ""
         for dicc in self.tag:
             exit += str("\n" + dicc['tag'] + "\t")
@@ -20,8 +32,12 @@ class Print (smallsmilhandler.smallSMILHandler):
                     exit += str(key + "=\"" + dicc[key] + "\"\t")
         print(exit)
 
-    def print_json(self):
-        with open('karaoke.json', 'w') as json_file:
+    def to_json(self, json_name=''):
+        file = sys.argv[1]
+        if json_name == '':
+            name = file[:file.rfind('.')]
+            json_name = name + '.json' 
+        with open(json_name, 'w') as json_file:
             json.dump(self.tag, json_file)
             letters = json.dumps(self.tag)
             print(letters)
@@ -37,17 +53,11 @@ class Print (smallsmilhandler.smallSMILHandler):
 
 if __name__ == "__main__":
 
-    parser = make_parser()
-    cHandler = Print()
-    parser.setContentHandler(cHandler)
-
-    fichero = sys.argv[1]
-
     try:
-        parser.parse(open(fichero))
+        karaoke = KaraokeLocal()
     except (FileNotFoundError, IndexError):
         sys.exit("Usage: python3 karaoke.py file.smil")
 
-    cHandler.print_line()
-    cHandler.print_json()
-    cHandler.download()
+    karaoke.__str__()
+    karaoke.to_json()
+    karaoke.download()
